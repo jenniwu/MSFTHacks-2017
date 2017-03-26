110209d3a1a73428fce7c41f917d940353a28780
Natalie Ngan <nataliehyn22@gmail.com>
2017-03-25 23:47:34 -0700

    dialog added

--- app.js
+++ app.js
@@ -19,11 +19,80 @@ var connector = new builder.ChatConnector({
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
+var intents = new builder.IntentDialog();
+//bot.dialog('/', intents);
+
+
 //=========================================================
 // Bots Dialogs
 //=========================================================
 
-bot.dialog('/', function (session) {
-    session.send("Hello World");
-    session.send(session.message.text.length);
-});
+// bot.dialog('/', function (session) {
+//     session.send("Hello World");
+//     session.send("Bye :P");
+// });
+
+var diet;
+var allergy;
+
+
+bot.dialog('/', [
+    function (session) {
+        session.beginDialog('/askItem');
+    },
+    function (session, results) {
+        session.send('You entered ' + session.message.text + '.', results.response.text);
+        session.beginDialog('/askDiet');
+    }
+]);
+bot.dialog('/askItem', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What\'s in your grocery basket?');
+    },
+    function (session, results) {
+        session.endDialogWithResult(results);
+    }
+]);
+
+bot.dialog('/askDiet', [
+    function (session) {
+        builder.Prompts.text(session, 'Do you have any dietary restrictions?');
+    },
+    function (session, results) {
+        diet = response;
+        session.endDialogWithResult(results);
+    }
+]);
+
+
+
+// bot.dialog('/', [
+//     function (session) {
+//         session.beginDialog('/askName');
+//     }
+
+// ]);
+// bot.dialog('/askItem', [
+//     function (session) {
+//         builder.Prompts.text(session, 'Hi! What is in your grocery basket?');
+//     },
+//     function (session, results) {
+//         session.send("You mentioned " + results.text);
+//         session.endDialogWithResult(results);
+//     }
+
+// ]);
+
+// var intents = new builder.IntentDialog();
+// bot.dialog('/askDiet', intents);
+
+// intents.matches(/^echo/i, [
+//     function (session) {
+//         builder.Prompts.text(session, "Do you have any dietary restrictions?");
+//     },
+//     function (session, results) {
+//         diet = session.message.text;
+//         session.send("Ok... %s", results.response);
+//     }
+// ]);
+
